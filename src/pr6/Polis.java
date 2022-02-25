38295f690bbb107828b0836b96b2d94bb9d7446c
PozhiloyKrasavec <lushago123@gmail.com>
2022-02-25 09:21:25 +0300

    initial commit

---
+++ src/pr6/Polis.java
@@ -0,0 +1,20 @@
+package pr6;
+
+
+public class Polis {
+    int id;
+    String holderSurname;
+
+    public Polis(int id, String holderSurname) {
+        this.id = id;
+        this.holderSurname = holderSurname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getHolderSurname() {
+        return holderSurname;
+    }
+}
